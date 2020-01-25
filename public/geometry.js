9cb4d3d17cc4954f27de9291989dc87f33882a5b
Charles Javelona <charlesjavelona@gmail.com>
2020-01-25 18:13:37 -0500

    Made 3 rotating cubes

--- public/geometry.js
+++ public/geometry.js
@@ -1,22 +1,61 @@
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const main = () => {
+  const canvas = document.querySelector('#canvas');
+  const renderer = new THREE.WebGLRenderer({canvas});
 
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+  const fov = 75;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
 
-let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-let  cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+  const scene = new THREE.Scene();
 
-camera.position.z = 5;
+  {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
 
-const animate = () => {
-  requestAnimationFrame(animate)
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera)
-}
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-animate()
+  function makeInstance(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({color});
+
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    cube.position.x = x;
+
+    return cube;
+  }
+
+  const cubes = [
+    makeInstance(geometry, 0x8844aa, -2),
+    makeInstance(geometry, 0x44aa88, 0),
+    makeInstance(geometry, 0xaa8844, 2),
+  ];
+
+  function render(time) {
+    time *= 0.001; // convert time to seconds
+
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * 0.1;
+      const rotation = time * speed;
+      cube.rotation.x = rotation;
+      cube.rotation.y = rotation;
+    });
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+};
+
+main();
